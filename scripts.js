5bf63a711511a1aee67f27d5ae541798acc26a49
sillypolls <mittenzwei-emmett@el.gstboces.org>
2025-04-01 10:45:27 -0400

    the commit of the hour
    how to center a div:tm:

--- scripts.js
+++ scripts.js
@@ -12,11 +12,29 @@ applySystemTheme();
 
 const centeredText1 = document.querySelector('.centered-text1');
 centeredText1.addEventListener('click', () => {
-    centeredText1.classList.toggle('clicked');
-    result1++
+    centeredText1.classList.toggle('clicked1');
 });
 const centeredText2 = document.querySelector('.centered-text2');
-centeredText1.addEventListener('click', () => {
-    centeredText2.classList.toggle('clicked');
-    result2++
+centeredText2.addEventListener('click', () => {
+    centeredText2.classList.toggle('clicked2');
 });
+const centeredText3 = document.querySelector('.centered-text3');
+centeredText3.addEventListener('click', () => {
+    centeredText3.classList.toggle('clicked3');
+})
+const centeredText4 = document.querySelector('.centered-text4');
+centeredText4.addEventListener('click', () => {
+    centeredText4.classList.toggle('clicked4');
+});
+const centeredText5 = document.querySelector('.centered-text5');
+centeredText5.addEventListener('click', () => {
+    centeredText5.classList.toggle('clicked5');
+});
+const centeredText6 = document.querySelector('.centered-text6');
+centeredText6.addEventListener('click', () => {
+    centeredText6.classList.toggle('clicked6');
+})
+const centeredText7 = document.querySelector('.centered-text7');
+centeredText7.addEventListener('click', () => {
+    centeredText7.classList.toggle('clicked7');
+})
